19bea570797be7b27087e0ec7f7cb1d39122b49f
CormacDaly <cormacdaly24@gmail.com>
2023-10-17 16:26:32 +0100

    escrow.sol basic layout started

--- src/escrow.sol
+++ src/escrow.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 contract Escrow {
+
+    // state variables
+
+    // constructors
+    constructor() Ownable(msg.sender) {
+        // logic here
+    }
     
+    // functions
+    function deposit(address tokenAddress, uint256 amount) public {
+        // logic here
+    }
+
 }
