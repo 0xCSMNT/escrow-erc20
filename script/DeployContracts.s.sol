23a92801c7547f37f90e0983d8101c6d67c62065
CormacDaly <cormacdaly24@gmail.com>
2023-11-08 16:09:09 +0000

    test file working with deploy script, first test to check verifier address on escrow contract created

---
+++ script/DeployContracts.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MockLinkToken} from "../src/TestTokens.sol";
+import {MockUniToken} from "../src/TestTokens.sol";
+import {Verifier} from "../src/verifier.sol";
+import {Escrow} from "../src/escrow.sol";
+
+contract DeployContracts is Script {
+    // Declare the contracts as public state variables
+    MockLinkToken public mockLink;
+    MockUniToken public mockUni;
+    Escrow public escrow;
+    Verifier public verifier;
+
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the mock tokens
+        mockLink = new MockLinkToken();
+        mockUni = new MockUniToken();
+
+        // Deploy the Escrow and Verifier contracts
+        escrow = new Escrow();
+        verifier = new Verifier(address(escrow));
+        
+        // Set up the Escrow contract with the verifier
+        escrow.setVerifier(address(verifier));
+
+        vm.stopBroadcast();
+
+        // Output the addresses of the deployed contracts
+        console.log("[IN DEPLOY] MockLinkToken deployed at:", address(mockLink));
+        console.log("[IN DEPLOY] MockUniToken deployed at:", address(mockUni));
+        console.log("[IN DEPLOY] Escrow deployed at:", address(escrow));
+        console.log("[IN DEPLOY] Verifier deployed at:", address(verifier));
+    }
+}
+
+
+
+
+
--- test/unit/VerifierTest.t.sol
+++ test/unit/VerifierTest.t.sol
@@ -3,18 +3,47 @@
 pragma solidity ^0.8.0;
 
 import {Verifier} from "src/verifier.sol";
+import {Escrow} from "src/escrow.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
+import {DeployContracts} from "script/DeployContracts.s.sol";
+import {MockLinkToken} from "src/TestTokens.sol";
+import {MockUniToken} from "src/TestTokens.sol";
 
 contract VerifierTest is StdCheats, Test {
     Verifier public verifier;
+    Escrow public escrow;
+    MockLinkToken public mockLink;
+    MockUniToken public mockUni;
 
     // CONSTANTS
     // like: uint constant public ZERO = 0;
 
     // SETUP FUNCTION 
+    function setUp() external {
+        DeployContracts deployer = new DeployContracts();
+        deployer.run();
+
+        // Initialize the deployed contracts
+        escrow = deployer.escrow();
+        verifier = deployer.verifier();
+        mockLink = deployer.mockLink();
+        mockUni = deployer.mockUni();
+    }
 
     // TEST FUNCTIONS
+
+    // test verifier address set correctly
+    function testVeriferAddressOnEscrow() public {
+        address escrowVerifierAddress = escrow.viewVerifierAddress();
+        address verifierAddress = address(verifier);
+        assertEq(escrowVerifierAddress, verifierAddress);
+        console.log("[IN TEST] Verifier address: ", verifierAddress);
+        console.log("[IN TEST] Verifier address on Escrow contract: ", escrowVerifierAddress);
+        
+    }
+
+    // test balances of tokens for user accounts
 
     // createDeal()
     // fundDeal()
