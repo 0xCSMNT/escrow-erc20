40ac06f4be623c67e0c01aabf9a4d7ae040cce84
CormacDaly <cormacdaly24@gmail.com>
2023-11-09 13:23:18 +0000

    test added to check balances for dummy token users

--- test/unit/VerifierTest.t.sol
+++ test/unit/VerifierTest.t.sol
@@ -17,7 +17,14 @@ contract VerifierTest is StdCheats, Test {
     MockUniToken public mockUni;
 
     // CONSTANTS
-    // like: uint constant public ZERO = 0;
+    uint256 public constant TOKEN_MINT_BALANCE = 100;
+    address public constant DEV_ACCOUNT_0 = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address public constant DEV_ACCOUNT_1 = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    
+    // VARIABLES
+    uint256 public expectedLinkBalance;
+    uint256 public expectedUniBalance;
+
 
     // SETUP FUNCTION 
     function setUp() external {
@@ -29,6 +36,10 @@ contract VerifierTest is StdCheats, Test {
         verifier = deployer.verifier();
         mockLink = deployer.mockLink();
         mockUni = deployer.mockUni();
+
+        // Set expected balances using decimals
+        expectedLinkBalance = TOKEN_MINT_BALANCE * (10 ** uint256(mockLink.decimals()));
+        expectedUniBalance = TOKEN_MINT_BALANCE * (10 ** uint256(mockUni.decimals()));
     }
 
     // TEST FUNCTIONS
@@ -37,13 +48,32 @@ contract VerifierTest is StdCheats, Test {
     function testVeriferAddressOnEscrow() public {
         address escrowVerifierAddress = escrow.viewVerifierAddress();
         address verifierAddress = address(verifier);
-        assertEq(escrowVerifierAddress, verifierAddress);
+        assertEq(escrowVerifierAddress, verifierAddress, "Verifer address mismatch");
         console.log("[IN TEST] Verifier address: ", verifierAddress);
         console.log("[IN TEST] Verifier address on Escrow contract: ", escrowVerifierAddress);
         
     }
 
     // test balances of tokens for user accounts
+    // dev account 0 should have 100e18 mLINK
+    // dev account 1 should have 100e18 mUNI
+    function testAccountBalancesForDummyTokens() public {
+        
+        // dev account addresses
+        address devAccount0 = DEV_ACCOUNT_0;
+        address devAccount1 = DEV_ACCOUNT_1;
+
+        // balances of dev accounts
+        uint256 devAccount0LinkBalance = mockLink.balanceOf(devAccount0);
+        uint256 devAccount1UniBalance = mockUni.balanceOf(devAccount1);
+        console.log("[IN TEST] Dev Account 0 LINK balance: ", devAccount0LinkBalance);
+        console.log("[IN TEST] Dev Account 1 UNI balance: ", devAccount1UniBalance);
+
+        // test balances
+        assertEq(devAccount0LinkBalance, expectedLinkBalance, "incorrect LINK balance for dev account 0");
+        assertEq(devAccount1UniBalance, expectedUniBalance, "incorrect UNI balance for dev account 1");
+        
+    }
 
     // createDeal()
     // fundDeal()
